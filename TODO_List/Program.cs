37b32705c6c3424a6fa6f2aaf1c118a177d6dacb
Nat386 <n.karasyova53@gmail.com>
2024-03-03 16:34:47 -0600

    todolist_final

--- TODO_List/Program.cs
+++ TODO_List/Program.cs
@@ -2,44 +2,67 @@
 
 class Program
 {
+  
     static void Main()
     {
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.Unicode;
-        // В этом задании вам нужно создать список дел.
-        // Исплоьзуйте массив или лист для хранения списка дел.
-        // При запуске программа должна выводить меню на экран и ждать дальнейших действий пользователя.
-        // После выполнения действия программа должна снова выводить меню и ждать дальнейших действий пользователя.
-        // Все действия должны выполняться до тех пор, пока пользователь не выберет пункт "Выход".
-        
-        
+     
+           
+        List<string> toDoList = new List<string>();
+
+
         while (true)
         {
-            Console.WriteLine("Список дел:");
-            // Выводим список задач
-            
-            // Выводим меню
-            
-            // Считываем выбор пользователя
-            
-            // Обрабатываем выбор пользователя
-            
-            
-            // 1. Добавить задачу
-                // Считываем задачу
-                // Добавляем задачу
-            
-            
-            // 2. Удалить задачу
-                // Считываем номер задачи
-                // Удаляем задачу
-                // Если ввод некорректный, выводим сообщение об ошибке
+            Console.WriteLine("To Do List");
+            Console.WriteLine($"Here is the list of things you need to do:");
+            foreach (string task in toDoList)
+            {                
+                Console.WriteLine("- " + task);
+            }
+
+            Console.WriteLine($"Please choose the option from the menu:");
+            Console.WriteLine($"1. Add the task");
+            Console.WriteLine($"2. Remove the task");
+            Console.WriteLine($"3. Exit");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine($"Please enter the task that you want to add to the list.");
+                    string newTask = Console.ReadLine();
+                    toDoList.Add(newTask);
+                    break;
+
+                case 2:
+                    Console.WriteLine($"Please enter the task that you want to remove from the list.");
+                    int taskNum = int.Parse(Console.ReadLine());
+
+                    if (taskNum >= 1 && taskNum <= toDoList.Count)
+                    {
+                        toDoList.RemoveAt(taskNum - 1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error.Please choose a valid task number.");
+                    }
+                    break;
+
+
+                case 3:
+                    Console.WriteLine($"Thank you using our program. See you next time!");
+                    return;
+
+
+                default:
+                    Console.WriteLine($"Please choose the correct option from 1 to 3");
+                    break;
+            }
+                   
                 
-            // 3. Выход
-                // Выходим из программы
-                
-            // Ввод за пределами меню
-                // Выводим сообщение об ошибке
+           
+         
         }
     }
 }
